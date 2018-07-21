1696abf1fd6ab673e7054fa782f1a0813e4892e2
Harold Davidson <harold.davidson89@gmail.com>
2018-07-20 19:05:25 -0500

    adding more content

---
+++ variableRecap.playground/Contents.swift
@@ -0,0 +1,22 @@
+// BELOW IS A QUicK CODING CHALLENGE TO JOG YOUR MEMORY OF THINGS WE COVERED LAST LESSON
+
+
+
+// create a variable called age and assign it to your current age
+
+
+
+// create a constant called birth year and assign it to the year you were born  (remember to use camel case)
+
+
+
+// write a comment below and explain why age is a variable and birth year is a constant
+
+
+
+// add 1 to your age variable and assign it to a variable called birthday
+
+
+
+// you have $34 and milk costs $4 a gallon. You need to buy as much milk as you can. Create 2 constants and use the modulus operator find out how much money you'll have left after buying all the milk that you can. Assign the value to a constant called moneyLeftOver and print moneyLeftOver to the console.
+
