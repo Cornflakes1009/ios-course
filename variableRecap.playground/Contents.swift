7f64a3e3ec2d7150701780a147e77bc66850c0c8
Harold Davidson <harold.davidson89@gmail.com>
2018-07-21 00:39:28 -0500

    added arrays

--- variableRecap.playground/Contents.swift
+++ variableRecap.playground/Contents.swift
@@ -1,22 +1,34 @@
-// BELOW IS A QUicK CODING CHALLENGE TO JOG YOUR MEMORY OF THINGS WE COVERED LAST LESSON
+// BELOW IS A QUICK CODING CHALLENGE TO JOG YOUR MEMORY OF THINGS WE COVERED LAST LESSON
 
 
 
 // create a variable called age and assign it to your current age
-
+var age = 45
 
 
 // create a constant called birth year and assign it to the year you were born  (remember to use camel case)
-
+let birthYear = 1972
 
 
 // write a comment below and explain why age is a variable and birth year is a constant
-
+// var for age because age changes and birth year is a constant because it doesn't change
 
 
 // add 1 to your age variable and assign it to a variable called birthday
+age += 1
 
-
+print(age)
 
 // you have $34 and milk costs $4 a gallon. You need to buy as much milk as you can. Create 2 constants and use the modulus operator find out how much money you'll have left after buying all the milk that you can. Assign the value to a constant called moneyLeftOver and print moneyLeftOver to the console.
+let moneyHave = 34
+let costOfMilk = 4
+let moneyLeftover = moneyHave % costOfMilk
+print(moneyLeftover)
+
+
+
+
+
+
+
 
